57604b11efe47597800e54144a564e2065f311a6
Lorenzo <lorenzoimbastari@gmail.com>
2026-09-16 21:44:33 +0200

    1.1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "com.aria.ariacast"
         minSdk = 31
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.1.7"
+        versionCode = 15
+        versionName = "1.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
